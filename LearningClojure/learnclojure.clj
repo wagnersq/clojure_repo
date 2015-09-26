339cf9e9aeb846a7cd642496772b12150e87f911
Wagner Queiroz <wagnersq@gmail.com>
2015-09-26 00:41:14 -0300

    Vector, List, Set and Map definition

--- LearningClojure/learnclojure.clj
+++ LearningClojure/learnclojure.clj
@@ -4,7 +4,7 @@
 ; function
 (print "Hello world")
 
-; math function
+; Math operation
 (+ 1 1)
 (- 2 1)
 (* 2 4)
@@ -17,22 +17,46 @@
 (type true)
 (type "Hello")
 
-; Macro
+; Macro or Map
 (type :a)
 (type (keyword "a"))
 
+; Symbol
 (type (quote a))
-(type 'a')
+(type 'a)
 
 ; List definition
-(type '(1 2 3))
+(type '(1 2 3)) ; linked list
 (type (list 1 2 3))
 (type '(print 2 3))
 (type (print 2 3))
 
+; Vector
+(type (vector 3 2 1)) ; array
+(type [3 2 1])        ; array
 
+; Value of N-th array position
+(nth (vector 9 5 13) 1)
+
+; Value of first item of list
+(first '(8 0 -9 3))
+
+; Value of last item of list
+(last '(8 0 -9 3 7))
+
+; Map
+{:a 0, :b 4, :c 2}
+{:a {:b -1, :c 1}}
+(type (hash-map :a 1 :b 2))
+
+; Set
+(type #{1 2 3})
+(type (hash-set 1 4 5))
+
+; Binding definition
 (defn x [a b]
   (let [ab (* a b)]
     (+ ab 3)))
 
 (x 4 3)
+
